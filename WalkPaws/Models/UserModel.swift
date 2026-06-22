22b27c07705683e44e18b0ea91a45325fb4e998d
sofiabarrientosr29 <sofiabarrientosr29@gmail.com>
2026-06-22 22:45:54 +0200

    Add proyect

---
+++ WalkPaws/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  WalkPaws
+//
+//  Created by Sofia Barrientos Raszkowska on 22/6/26.
+//
+
+import Foundation
---
+++ WalkPaws/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalkPaws
+//
+//  Created by Sofia Barrientos Raszkowska on 22/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
